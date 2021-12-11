49e6863b19e4215e15b37da374cdd9440d732747
sydneyjoa <SYDNEY.JOA@newhorizons.edu.do>
2021-12-11 21:21:39 +0000

    accomodations part

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReservationComponent } from './reservation/reservation.component';
 import { ButtonComponent } from './button/button.component';
+import { AccomodationsComponent } from './accomodations/accomodations.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ReservationComponent,
-    ButtonComponent
+    ButtonComponent,
+    AccomodationsComponent
   ],
   imports: [
     BrowserModule,
